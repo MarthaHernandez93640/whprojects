1d66e4d547c3c0868f5f279fd6469a1798dd4aa1
martha hernandez <poodle93622@gmail.com>
2018-04-13 13:47:49 -0700

    worked on story

---
+++ story/js/script.js
@@ -0,0 +1,11 @@
+var story=["the chinese emporium"];
+console.log(story);
+
+  var story = prompt('lucy spotted the sign down a small alleyway. Mum! Look! What is it? replied Lucy’s mum. “We need to get home. It’s so foggy out, it’s creepy. There’s an interesting little shop down that alleyway, Lucy continued. “Can we take a quick look? Pleeease? Maybe we’ll find a dressing table in there?”');
+    if(story=='ok'){
+        console.log('They go in to a cool shop with a ton of old items');
+    }else if(story=='no'){
+        console.log('lucy crys');
+    }else{
+        console.log('they go home');
+    }
